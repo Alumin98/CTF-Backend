f07d0e9d5426d3ce728391ef1866ce8f744682fa
Alumin98 <muhammadalamin271@gmail.com>
2025-05-14 15:53:13 +1000

    Fix main.py to ensure proper table creation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.models import * 
-
+from app.models import *  # ensures all models from __init__.py are loaded
 
 app = FastAPI()
 
