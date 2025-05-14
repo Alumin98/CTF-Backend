9738e7055062a24532f8f2338153619e8c74a182
Alumin98 <muhammadalamin271@gmail.com>
2025-05-14 15:48:01 +1000

    Fix main.py to ensure proper table creation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.models import user, role, team, team_member, category, challenge, hint, challenge_tag, event
+from app.models import * 
 
 
 app = FastAPI()
