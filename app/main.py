e326a7915f24634257def2e22a6d51d5f1c4cd18
Alumin98 <muhammadalamin271@gmail.com>
2025-05-14 13:05:15 +1000

    Fix foreign key error: reordered model imports

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.models import user, role, team
+from app.models import role, user, team
 
 app = FastAPI()
 
--- app/models/_init_.py
+++ app/models/_init_.py
@@ -1,3 +1,4 @@
-from app.models.user import User
 from app.models.role import Role
+from app.models.user import User
 from app.models.team import Team
+
