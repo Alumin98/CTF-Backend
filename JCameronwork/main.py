b83e351c1137fe2ba3728c1253f1e28b5abdb7bc
Yadarsan <161267994+Yadarsan@users.noreply.github.com>
2025-05-19 22:11:50 +1000

    Update main.py

--- JCameronwork/main.py
+++ JCameronwork/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from teams import router as team_router
+from auth import router as auth_router
+from challenges import router as challenge_router
+from submissions import router as submission_router
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello from main.py"}
+    return {"message": "API is running."}
 
+app.include_router(auth_router, prefix="/auth")
+app.include_router(team_router)
+app.include_router(challenge_router)
+app.include_router(submission_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
