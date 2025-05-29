5950402d9b2860e07fdbf6247d7ce8f835b0cfb7
sahanjayd <162644845+sahanjayd@users.noreply.github.com>
2025-05-30 01:44:12 +1000

    Update challenges.py
    Admin Permission on Sensitive Routes

--- app/routes/challenges.py
+++ app/routes/challenges.py
@@ -1,38 +1,37 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import AsyncSession
 from sqlalchemy.future import select
 from app.models.challenge import Challenge
 from app.database import get_db
 from app.schemas import ChallengeCreate, ChallengePublic
-from app.auth_token import get_current_user
+from app.auth_token import require_admin
+from app.routes.auth import hash_flag
 
 router = APIRouter()
-
 
 @router.post("/challenges/", response_model=ChallengePublic)
 async def create_challenge(
     challenge: ChallengeCreate,
-    db: Session = Depends(get_db),
-    user=Depends(get_current_user)
+    db: AsyncSession = Depends(get_db),
+    user=Depends(require_admin),
 ):
-    # TODO: add admin check here later
-    new_challenge = Challenge(**challenge.dict())
+    challenge_data = challenge.dict()
+    challenge_data['flag'] = hash_flag(challenge.flag)
+    new_challenge = Challenge(**challenge_data)
     db.add(new_challenge)
-    db.commit()
-    db.refresh(new_challenge)
+    await db.commit()
+    await db.refresh(new_challenge)
     return new_challenge
 
-
 @router.get("/challenges/", response_model=list[ChallengePublic])
-async def list_challenges(db: Session = Depends(get_db)):
-    result =  db.execute(select(Challenge))
+async def list_challenges(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Challenge))
     challenges = result.scalars().all()
     return challenges
 
-
 @router.get("/challenges/{challenge_id}", response_model=ChallengePublic)
-async def get_challenge(challenge_id: int, db: Session = Depends(get_db)):
-    result = db.execute(select(Challenge).where(Challenge.id == challenge_id))
+async def get_challenge(challenge_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Challenge).where(Challenge.id == challenge_id))
     challenge = result.scalar()
     if not challenge:
         raise HTTPException(status_code=404, detail="Challenge not found")
