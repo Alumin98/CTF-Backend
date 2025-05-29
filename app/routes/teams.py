dcfb110862947cd23441d84c09a430521345deb1
sahanjayd <162644845+sahanjayd@users.noreply.github.com>
2025-05-30 01:58:09 +1000

    Update teams.py

--- app/routes/teams.py
+++ app/routes/teams.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.models.team import Team
 from app.models.user import User
