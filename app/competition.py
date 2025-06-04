ee0aa601266ae0c67e34c09edf0dee726ed58fda
Chamithaar <163808219+Chamithaar@users.noreply.github.com>
2025-06-04 22:20:40 +1000

    Update competition.py

--- app/competition.py
+++ app/competition.py
@@ -1,1 +1,11 @@
+from pydantic import BaseModel
+
+class CompetitionCreate(BaseModel):
+    name: str
+
+class CompetitionOut(CompetitionCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
 
